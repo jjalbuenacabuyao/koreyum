e7650939e72b12bb6e4176f4567686bbd097e75e
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-12-17 10:26:51 +0800

    re-arrange the inputs

--- components/registerModal.php
+++ components/registerModal.php
@@ -7,31 +7,26 @@ echo '<div class="modal" tabindex="-1" id="register">
       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
     <div class="modal-body">
-      <form action="php/userInsert.php" method="post">
-
-        <div class="mb-3">
-          <label for="cusId" class="form-label">Username</label>
-          <input name="customerId" type="text" class="form-control" id="cusId" aria-describedby="emailHelp" required />
-        </div>
-        <div class="mb-3">
-          <label for="password" class="form-label">Password</label>
-          <input name="password" type="password" class="form-control" id="password" aria-describedby="emailHelp" required />
-        </div>
-        <div class="mb-3">
-          <label for="fullName" class="form-label">Full Name</label>
-          <input name="fullName" type="text" class="form-control" id="fullName" aria-describedby="emailHelp" required />
+      <form action="php/register.php" method="post">
+      <div class="mb-3">
+          <label for="fullname" class="form-label">Full Name</label>
+          <input name="fullname" type="text" class="form-control" id="fullname" aria-describedby="emailHelp" required />
         </div>
         <div class="mb-3">
           <label for="address" class="form-label">Address</label>
           <input name="address" type="text" class="form-control" id="address" aria-describedby="emailHelp" required />
         </div>
         <div class="mb-3">
-          <label for="contactNo" class="form-label">Contact number</label>
-          <input name="contactNo" type="text" class="form-control" id="contactNo" aria-describedby="emailHelp" required />
+          <label for="contact-number" class="form-label">Contact number</label>
+          <input name="contact-number" type="text" class="form-control" id="contact-number" aria-describedby="emailHelp" required />
         </div>
         <div class="mb-3">
-          <label for="Email" class="form-label">Email Address</label>
-          <input name="Email" type="Email" class="form-control" id="Email" aria-describedby="emailHelp" required />
+          <label for="email" class="form-label">Email Address</label>
+          <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp" required />
+        </div>
+        <div class="mb-3">
+          <label for="password" class="form-label">Password</label>
+          <input name="password" type="password" class="form-control" id="password" aria-describedby="emailHelp" required />
         </div>
         <div>
           <input type="submit" name="submit" class="btn btn-primary">
@@ -40,5 +35,4 @@ echo '<div class="modal" tabindex="-1" id="register">
     </div>
   </div>
 </div>
-</div>'
-?>
+</div>';
