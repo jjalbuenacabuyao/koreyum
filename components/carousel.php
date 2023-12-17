e9bb7d50fae719ebf5afd9035ad523ae16df6a33
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-12-17 10:32:03 +0800

    remove echo

--- components/carousel.php
+++ components/carousel.php
@@ -1,5 +1,4 @@
-<?php 
-echo '<div id="carouselExampleRide" class="carousel slide" data-bs-ride="carousel">
+<div id="carouselExampleRide" class="carousel slide" data-bs-ride="carousel">
 <div class="carousel-inner rounded">
   <div class="carousel-item active w-100" data-bs-interval="3000">
     <img src="./assets/images/bg.png" class="d-block w-100 object-cover carousel-img" alt="...">
@@ -11,5 +10,4 @@ echo '<div id="carouselExampleRide" class="carousel slide" data-bs-ride="carouse
     <img src="./assets/images/unli-pork-samgyup.jpg" class="d-block w-100 object-cover carousel-img" alt="...">
   </div>
 </div>
-</div>'
-?>
+</div>
