21fced162e654dc457e68a40eef7d5c60c1657a6
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-12-18 20:13:48 +0800

    display errors

--- components/signInModal.php
+++ components/signInModal.php
@@ -1,3 +1,11 @@
+<?php
+$url = "http://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+$url_components = parse_url($url);
+if (isset($url_components['query'])) {
+  parse_str($url_components['query'], $params);
+}
+?>
+
 <div class="modal" tabindex="-1" id="sign-in">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
@@ -10,19 +18,25 @@
           <div>
             <label for="email" class="form-label"></label>
             <input name="email" type="email" class="form-control" id="email" placeholder="Email">
+            <?php
+            if (isset($params["error"]) && $params["error"] === "User does not exist") {
+              echo '<p class="text-danger">User does not exist</p>';
+            } ?>
           </div>
           <div class="input-box">
             <label for="password" class="form-label"></label>
             <input name="password" type="password" class="form-control" id="password" placeholder="Password">
+            <?php
+            if (isset($params["error"]) && $params["error"] === "Incorect password") {
+              echo '<p class="text-danger">Incorect password</p>';
+            } ?>
             <img src="  ./assets/images/eycon.png" id="eye" width="30" height="auto">
-          </div><br>
-          <div>
-            <input type="submit" name="submit" class="btn btn-primary">
           </div>
         </form>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
       </div>
     </div>
   </div>
