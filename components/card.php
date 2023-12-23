4836037303810c4d8f1b8d67d82c755736ec07c7
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-12-23 18:45:00 +0800

    set the color of font to red and add font weight

--- components/card.php
+++ components/card.php
@@ -5,7 +5,7 @@ function renderCard($withButton, $imageSrc, $title, $price)
     echo '<div class="card shadow">
     <img src=' . $imageSrc . ' class="card-img-top" alt="...">
     <div class="card-body">
-      <h4 class="card-title">' . $title . '</h4>
+      <h4 class="card-title font-secondary text-red-600 fw-bold">' . $title . '</h4>
       <p class="card-text">Includes choice of 2 meat variants (500grams each), Braised Tofu, Korean Kimchi, Lettuce, Cucumber, Garlic, Ssamjang,Gochujang, Cheese Sauce and Potato Marbbles.</p>
       <p class="fw-bold">₱' . $price . '.00</p>
       <a href="#" class="btn btn-primary">Order</a>
@@ -17,7 +17,7 @@ function renderCard($withButton, $imageSrc, $title, $price)
   echo '<div class="card shadow">
   <img src=' . $imageSrc . ' class="card-img-top" alt="...">
   <div class="card-body">
-    <h4 class="card-title">' . $title . '</h4>
+    <h4 class="card-title font-secondary text-red-600 fw-bold">' . $title . '</h4>
     <p class="fw-bold">₱' . $price . '.00</p>
   </div>
 </div>';
