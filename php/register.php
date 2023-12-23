039c25fe6b2fec6e3a81885590ba2b16a99b44e6
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-12-23 13:25:58 +0800

    redirect if the email already exist

--- php/register.php
+++ php/register.php
@@ -9,15 +9,23 @@ if (isset($_POST['submit'])) {
 	$address = $_POST['address'];
 	$contactNumber = $_POST['contact-number'];
 
+	$sql = "SELECT * FROM user WHERE email='$email'";
+	$result = mysqli_query($conn, $sql);
+
+	if (mysqli_num_rows($result)) {
+		header("Location: ../pages/register.php?error=email already exist");
+		exit();
+	}
+
 	$sql = "INSERT INTO user (id, email, password, fullname, address, contactNumber) VALUES (NULL, '$email', '$password', '$fullname', '$address', '$contactNumber')";
 
 	$result = mysqli_query($conn, $sql);
 
 	if ($result) {
-		echo "Registered";
-		header("Location: ../homepage.php?Registered");
+		header("Location: ../pages/login.php");
 	} else {
 		echo "Did not Register";
-		exit();
 	}
+
+	exit();
 }
