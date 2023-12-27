1f00efead9c8e5692b69fa671535c37a3616c480
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-12-27 20:52:49 +0800

    disable scrolling when dialog is open

--- assets/js/toggleDialog.js
+++ assets/js/toggleDialog.js
@@ -1,19 +1,25 @@
-let orderDialog = document.querySelector("#orderDialog");
-let reservationDialog = document.querySelector("#reservationDialog")
-let openReservationDialogButton = document.querySelectorAll("[data-trigger='reserve']");
-let openOrderDialogButton = document.querySelectorAll("[data-trigger='order']");
-let closeReservationDialogButton = document.querySelectorAll("[data-trigger='closeReservation']");
-let closeOrderDialogButton = document.querySelectorAll("[data-trigger='closeOrder']");
+import {
+  orderDialog,
+  reservationDialog,
+  openOrderDialogButton,
+  openReservationDialogButton,
+  closeOrderDialogButton,
+  closeReservationDialogButton
+} from "./index.js";
+
+const body = document.querySelector("[data-menuBody]");
 
 openOrderDialogButton.forEach(button => {
   button.addEventListener("click", () => {
     orderDialog.showModal();
+    body.style.overflowY = "hidden";
   })
 })
 
 openReservationDialogButton.forEach(button => {
   button.addEventListener("click", () => {
     reservationDialog.showModal();
+    body.style.overflowY = "hidden";
   })
 })
 
@@ -29,14 +35,10 @@ closeOrderDialogButton.forEach(button => {
   })
 })
 
-const label = document.createElement("label");
-label.setAttribute("for", "set1");
-label.innerHTML = "KoreYum Set 1";
+orderDialog.addEventListener("close", () => {
+  body.style.overflowY = "auto";
+})
 
-const input = document.createElement("input");
-input.setAttribute("type", "radio")
-input.setAttribute("id", "set1")
-input.setAttribute("name", "set1")
-
-orderDialog.appendChild(input)
-orderDialog.appendChild(label)
+reservationDialog.addEventListener("close", () => {
+  body.style.overflowY = "auto";
+})
