cd03b33d92cbdbe20026bb784cd3dc9d8a0d7dec
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-12-28 19:16:57 +0800

    add semi-colon and spaces

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,9 @@
 const orderDialog = document.querySelector("#orderDialog");
-const reservationDialog = document.querySelector("#reservationDialog")
+const reservationDialog = document.querySelector("#reservationDialog");
+
 const openReservationDialogButton = document.querySelectorAll("[data-trigger='reserve']");
 const openOrderDialogButton = document.querySelectorAll("[data-trigger='order']");
+
 const closeReservationDialogButton = document.querySelectorAll("[data-trigger='closeReservation']");
 const closeOrderDialogButton = document.querySelectorAll("[data-trigger='closeOrder']");
 
