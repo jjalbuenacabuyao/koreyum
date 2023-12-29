1ed691c815236d1a8257cf6915dc3ac4436f602c
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-12-29 19:38:30 +0800

    add header

--- pages/orders.php
+++ pages/orders.php
@@ -26,7 +26,71 @@ if (!isset($userId)) {
 </head>
 
 <body class="font-primary mb-5">
-  <main class="container">
+  <header class="container">
+    <nav class="navbar navbar-expand-lg bg-white nav">
+      <div class="container-fluid p-0">
+        <a class="navbar-brand" href="#">
+          <img src="../assets/images/logo.png" width="128" height="auto" alt="KoreYum" />
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse flex-grow-0" id="navbarNavAltMarkup">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active text-dark" aria-current="page" href="../index.php">Home</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="./menu.php">Menu</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="./about.php">About</a>
+            </li>
+
+            <?php
+            if (!isset($_SESSION['id'])) {
+              echo '
+            <li class="nav-item" style="padding-right: 0.5rem;">
+              <a class="btn bg-red-600 text-white fw-bold rounded-pill px-3" style="--bs-btn-hover-bg: #991b1b;" href="./login.php">
+                Log in
+              </a>
+            </li>
+            
+            <li class="nav-item">
+              <a class="btn btn-outline-dark rounded-pill px-3" href="./register.php">
+                Register
+              </a>
+            </li>
+            ';
+            } else {
+              echo '
+              <li class="nav-item">
+                <a class="nav-link text-dark" href="#">My Orders</a>
+              </li>
+              ';
+
+              if ($_SESSION['id'] === "1") {
+                echo '
+              <li class="nav-item" style="padding-right: 0.5rem;">
+                <a class="nav-link text-dark" href="./ViewInvoice.php">View Daily Sales</a>
+              </li>';
+              }
+
+              echo '
+              <li class="nav-item">
+                <a href="../php/logout.php" class="btn btn-danger rounded-pill px-3 fw-bold">Log out</a>
+              </li>';
+            }
+            ?>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <main class="container mt-5">
     <h1 class="fw-bold font-secondary mb-3 text-center">My Orders</h1>
     <?php
     include "../php/db.php";
